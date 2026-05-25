30b62b6ecf87a3db3412c95929e78461ecfc33d3
oomaar <omar16694@gmail.com>
2026-05-25 20:04:24 +0300

    fix: update import path for TabId in SettingsClient component

--- src/components/admin/settings-client/settings-client.tsx
+++ src/components/admin/settings-client/settings-client.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import { useState } from "react";
-import type { TabId } from "./tabs/types/TabId";
+import type { TabId } from "./tabs/settings-client-tabs/types/TabId";
 import { SettingsClientHeader } from "./components/settings-client-header";
-import { SettingsProfileTab } from "./tabs/settings-profile-tab";
-import SettingsClientTabs from "./components/settings-client-tabs";
-import { SettingsAppearanceTab } from "./tabs/settings-appearance-tab";
-import { SettingsSecurityTab } from "./tabs/settings-security-tab";
-import { SettingsNotificationsTab } from "./tabs/settings-notifications-tab";
-import { SettingsIntegrationsTab } from "./tabs/settings-integrations-tab";
+import { SettingsClientTabs } from "./tabs/settings-client-tabs/settings-client-tabs";
+import { SettingsProfileTab } from "./tabs/settings-profile-tab/settings-profile-tab";
+import { SettingsAppearanceTab } from "./tabs/settings-appearance-tab/settings-appearance-tab";
+import { SettingsSecurityTab } from "./tabs/settings-security-tab/settings-security-tab";
+import { SettingsNotificationsTab } from "./tabs/settings-notifications-tab/settings-notifications-tab";
+import { SettingsIntegrationsTab } from "./tabs/settings-integrations-tab/settings-integrations-tab";
 
 export const ONE_YEAR = 60 * 60 * 24 * 365;
 
